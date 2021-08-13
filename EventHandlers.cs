037a7b86c1d519ffd09e6c155180ade676957419
AleRabo <83182793+AleRabo@users.noreply.github.com>
2021-08-13 17:23:00 +0200

    Update EventHandlers.cs

--- EventHandlers.cs
+++ EventHandlers.cs
@@ -1,59 +1,42 @@
-﻿using Exiled.Events.EventArgs;
+using Exiled.Events.EventArgs;
 using Exiled.API.Extensions;
 
 namespace WeaponWeight
 {
     public class EventHandlers
     {
-        public void OnChangingItem(ChangingItemEventArgs ev)
+        public void ChangingItem(ChangingItemEventArgs ev)
         {
-            if (ev.NewItem.id == ItemType.GunLogicer)
+            switch (ev.NewItem.id)
             {
-                ev.Player.ChangeWalkingSpeed(Plugin.Singleton.Config.LogicerWalkSpeed);
-                ev.Player.ChangeRunningSpeed(Plugin.Singleton.Config.LogicerRunSpeed);
+                case ItemType.GunLogicer:
+                    ev.Player.ChangeWalkingSpeed(Plugin.WeaponWeight.Config.LogicerWalkSpeed);ev.Player.ChangeRunningSpeed(Plugin.WeaponWeight.Config.LogicerRunSpeed); 
+                    break;
+                case ItemType.GunProject90:
+                    ev.Player.ChangeWalkingSpeed(Plugin.WeaponWeight.Config.Project90WalkSpeed);ev.Player.ChangeRunningSpeed(Plugin.WeaponWeight.Config.Project90RunSpeed); 
+                    break;
+                case ItemType.GunE11SR:
+                    ev.Player.ChangeWalkingSpeed(Plugin.WeaponWeight.Config.E11SRWalkSpeed);ev.Player.ChangeRunningSpeed(Plugin.WeaponWeight.Config.E11SRRunSpeed);
+                    break;
+                case ItemType.GunUSP:
+                    ev.Player.ChangeWalkingSpeed(Plugin.WeaponWeight.Config.USPWalkSpeed);ev.Player.ChangeRunningSpeed(Plugin.WeaponWeight.Config.USPRunSpeed);
+                    break;
+                case ItemType.GunMP7:
+                    ev.Player.ChangeWalkingSpeed(Plugin.WeaponWeight.Config.Mp7WalkSpeed);ev.Player.ChangeRunningSpeed(Plugin.WeaponWeight.Config.Mp7RunSpeed);
+                    break;
+                case ItemType.MicroHID:
+                    ev.Player.ChangeWalkingSpeed(Plugin.WeaponWeight.Config.MicroHIDWalkSpeed);ev.Player.ChangeRunningSpeed(Plugin.WeaponWeight.Config.MicroHIDRunSpeed);
+                    break;
+                case ItemType.GunCOM15:
+                    ev.Player.ChangeWalkingSpeed(Plugin.WeaponWeight.Config.Com15WalkSpeed);ev.Player.ChangeRunningSpeed(Plugin.WeaponWeight.Config.Com15RunSpeed);
+                    break;
             }
 
-            else if (ev.NewItem.id == ItemType.GunProject90)
-            {
-                ev.Player.ChangeWalkingSpeed(Plugin.Singleton.Config.Project90WalkSpeed);
-                ev.Player.ChangeRunningSpeed(Plugin.Singleton.Config.Project90RunSpeed);
-            }
-
-            else if (ev.NewItem.id == ItemType.GunMP7)
-            {
-                ev.Player.ChangeWalkingSpeed(Plugin.Singleton.Config.Mp7WalkSpeed);
-                ev.Player.ChangeRunningSpeed(Plugin.Singleton.Config.Mp7RunSpeed);
-            }
-
-            else if (ev.NewItem.id == ItemType.GunE11SR)
-            {
-                ev.Player.ChangeWalkingSpeed(Plugin.Singleton.Config.E11SRWalkSpeed);
-                ev.Player.ChangeRunningSpeed(Plugin.Singleton.Config.E11SRRunSpeed);
-            }
-
-            else if (ev.NewItem.id == ItemType.GunUSP)
-            {
-                ev.Player.ChangeWalkingSpeed(Plugin.Singleton.Config.USPWalkSpeed);
-                ev.Player.ChangeRunningSpeed(Plugin.Singleton.Config.USPRunSpeed);
-            }
-
-            else if (ev.NewItem.id == ItemType.GunCOM15)
-            {
-                ev.Player.ChangeWalkingSpeed(Plugin.Singleton.Config.Com15WalkSpeed);
-                ev.Player.ChangeRunningSpeed(Plugin.Singleton.Config.Com15RunSpeed);
-            }
-
-            else if (ev.NewItem.id == ItemType.MicroHID)
-            {
-                ev.Player.ChangeWalkingSpeed(Plugin.Singleton.Config.MicroHIDWalkSpeed);
-                ev.Player.ChangeRunningSpeed(Plugin.Singleton.Config.MicroHIDRunSpeed);
-            }
-
-            if (ev.NewItem.id != ItemType.GunLogicer && ev.NewItem.id != ItemType.GunProject90 && ev.NewItem.id != ItemType.MicroHID && ev.NewItem.id != ItemType.GunE11SR && ev.NewItem.id != ItemType.GunUSP && ev.NewItem.id != ItemType.GunCOM15 && ev.NewItem.id != ItemType.MicroHID && ev.NewItem.id != ItemType.GunMP7)
+            if (!ev.NewItem.id.IsWeapon() && ev.NewItem.id != ItemType.MicroHID && ev.NewItem.id == ItemType.None)
             {
                 ev.Player.ChangeWalkingSpeed(1f);
                 ev.Player.ChangeRunningSpeed(1f);
             }
         }
     }
-}
+} 
