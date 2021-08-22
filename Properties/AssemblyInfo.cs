ae44a58050df6f35e7206a2ed656790f1492ed38
AleRabo <83182793+AleRabo@users.noreply.github.com>
2021-08-22 12:59:38 +0200

    Update AssemblyInfo.cs

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,22 +5,22 @@ using System.Runtime.InteropServices;
 // General information about an assembly is controlled by the following
 // set of attributes. Change the values ​​of these attributes to change the information
 // associated with an assembly.
-[assembly: AssemblyTitle ("WeaponWeight")]
-[assembly: AssemblyDescription ("This Exiled plugin simulate the weight of holding a weapon")]
-[assembly: AssemblyConfiguration ("")]
-[assembly: AssemblyCompany ("")]
-[assembly: AssemblyProduct ("WeaponWeight")]
-[assembly: AssemblyCopyright ("Copyright © AleRabo 2021")]
-[assembly: AssemblyTrademark ("")]
-[assembly: AssemblyCulture ("")]
+[assembly: AssemblyTitle("WeaponWeight")]
+[assembly: AssemblyDescription("This Exiled plugin simulate the weight of holding a weapon")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WeaponWeight")]
+[assembly: AssemblyCopyright("Copyright © AleRabo 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
 
 // If you set ComVisible to false, the types in this assembly will not be visible
 // to the COM components. If you need to access a type in this assembly from
 // COM, set the ComVisible attribute for that type to true.
-[assembly: ComVisible (false)]
+[assembly: ComVisible(false)]
 
 // If the project is exposed to COM, the following GUID will be used as the type library ID
-[assembly: Guid ("123c0750-634d-4439-b3f7-aec9308b4f67")]
+[assembly: Guid("123c0750-634d-4439-b3f7-aec9308b4f67")]
 
 // The version information of an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all values ​​or set default values ​​for revision and build numbers
 // using the asterisk '*' as shown below:
 // [assembly: AssemblyVersion ("1.0. *")]
-[assembly: AssemblyVersion ("1.0.2.0")]
-[assembly: AssemblyFileVersion ("1.0.2.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
