ed313a48651b0a0df3b90f2028ecb55e7c33d8b5
AleRabo <83182793+AleRabo@users.noreply.github.com>
2021-08-22 12:58:34 +0200

    Updated to Exiled 3.0

--- Config.cs
+++ Config.cs
@@ -13,10 +13,10 @@ namespace WeaponWeight
         [Description("The running speed when a player have equiped the Logicer")]
         public float LogicerRunSpeed { get; set; } = 0.8f;
 
-        [Description("The walking speed when a player have equiped the Project 90")]
+        [Description("The walking speed when a player have equiped the Cross Vec")]
         public float Project90WalkSpeed { get; set; } = 0.95f;
 
-        [Description("The running speed when a player have equiped the Project 90")]
+        [Description("The running speed when a player have equiped the Cross Vec")]
         public float Project90RunSpeed { get; set; } = 1f;
 
         [Description("The walking speed when a player have equiped the MTF E11SR")]
@@ -25,10 +25,10 @@ namespace WeaponWeight
         [Description("The running speed when a player have equiped the MTF E11SR")]
         public float E11SRRunSpeed { get; set; } = 0.96f;
 
-        [Description("The walking speed when a player have equiped the USP")]
+        [Description("The walking speed when a player have equiped the GunCOM18")]
         public float USPWalkSpeed { get; set; } = 0.75f;
 
-        [Description("The running speed when a player have equiped the USP")]
+        [Description("The running speed when a player have equiped the GunCOM18")]
         public float USPRunSpeed { get; set; } = 0.85f;
 
         [Description("The walking speed when a player have equiped the Com-15")]
@@ -44,9 +44,27 @@ namespace WeaponWeight
         public float MicroHIDRunSpeed { get; set; } = 0.7f;
 
         [Description("The walking speed when a player have equiped the Mp7")]
-        public float Mp7WalkSpeed { get; set; } = 0.54f;
+        public float FSP9WalkSpeed { get; set; } = 0.54f;
 
-        [Description("The running speed when a player have equiped the Mp7")]
-        public float Mp7RunSpeed { get; set; } = 0.7f;
+        [Description("The running speed when a player have equiped the FSP9")]
+        public float FSP9RunSpeed { get; set; } = 0.7f;
+
+        [Description("The running speed when a player have equiped the AK")]
+        public float AKWalkSpeed { get; set; } = 0.7f;
+
+        [Description("The running speed when a player have equiped the AK")]
+        public float AKRunSpeed { get; set; } = 0.6f;
+
+        [Description("The running speed when a player have equiped the Revolver")]
+        public float RevolverWalkSpeed { get; set; } = 0.9f;
+
+        [Description("The running speed when a player have equiped the Revolver")]
+        public float RevolverRunSpeed { get; set; } = 0.8f;
+
+        [Description("The running speed when a player have equiped the ShotGun")]
+        public float ShotGunWalkSpeed { get; set; } = 0.8f;
+
+        [Description("The running speed when a player have equiped the ShotGun")]
+        public float ShotGunRunSpeed { get; set; } = 0.6f;
     }
 }
